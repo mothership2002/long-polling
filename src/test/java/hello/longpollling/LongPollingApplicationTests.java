b4f1ac05599e79cdafbeb09670da578ade9014d8
mothership <mothership2002@naver.com>
2023-08-18 22:30:26 +0900

    no message

---
+++ src/test/java/hello/longpollling/LongPollingApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.longpollling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongPollingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
